9f47b04ebcdffee62372dabf05a4e9288c33e90a
Saurav Sharma <54544042+Saurav60@users.noreply.github.com>
2025-01-25 02:40:14 +0530

    Delete daily_practice/missing_pandas.py

--- daily_practice/missing_pandas.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-def handle_missing_values(data):
-    df = pd.DataFrame(data)
-    for column in df.columns:
-        if df[column].isnull().any() and pd.api.types.is_numeric_dtype(df[column]):
-            median_value = df[column].median()
-            df[column].fillna(value=median_value, inplace=True)
-    return df
